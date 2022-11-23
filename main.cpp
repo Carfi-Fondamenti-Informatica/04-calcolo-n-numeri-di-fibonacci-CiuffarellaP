8836e30207dfe10534386f6686df7b9570d8cdfc
CiuffarellaP <117938895+CiuffarellaP@users.noreply.github.com>
2022-11-23 15:12:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+    int a=1,b=1,i=3,n=0;
+    cout<<"Fino a che numero vuoi sapere la sequenza Fibonacci? "<<endl;
+    cin>>n;
+    if (n>=2)
+    {cout<<a<<endl<<b<<endl;
+    while(i++ <=n){
+        cout<<a+b<<" "<<endl;
+        if(a<b){
+            a+=b;
+        }else
+        {b+=a;}
+    }
+    }else {cout<<"errore";}
+    cout<<endl;
    return 0;
 }
